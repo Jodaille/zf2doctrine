caac7b6825d49a49e3e490abb776e1946f2e7259
Jodaille <j-noury@legallais.com>
2014-12-12 11:49:48 +0100

    form to add a new catalog

--- module/Catalog/src/Catalog/Controller/IndexController.php
+++ module/Catalog/src/Catalog/Controller/IndexController.php
@@ -11,19 +11,74 @@ class IndexController extends AbstractActionController
 
     public function indexAction()
     {
-	$pageid        = $this->params()->fromPost('pageid', FALSE);
-	$catalog_id    = $this->params('id', 1);
+        // pageid received by POST method
+        $pageid        = $this->params()->fromPost('pageid', FALSE);
 
-	$catalogRepository = $this->getEntityManager()->getRepository('Catalog\Entity\Catalog');
-        
+        // id received by get method (url)
+        $catalog_id    = $this->params('id', 1);
+
+        // get Catalog repository
+        $catalogRepository = $this->getEntityManager()->getRepository('Catalog\Entity\Catalog');
+
+        // find catalog with id $catalog_id
         $catalog = $catalogRepository->find($catalog_id);
 
-        $factory 	= $this->getServiceLocator()->get('catalog');
-        $testCatalog 	= $factory->getCatalog($name = 'test');
+        // call service catalog to get an instance of Model/Catalog
+        $factory        = $this->getServiceLocator()->get('catalog');
 
-        return new ViewModel(array('catalog' => $catalog,
-				   'testCatalog' => $testCatalog,
-				  ));
+        // get catalog with name 'test'
+        $testCatalog    = $factory->getCatalog($name = 'test');
+
+        // assign result to view
+        return new ViewModel(
+                            array(
+                                'catalog'     => $catalog,
+                                'testCatalog' => $testCatalog,
+                                )
+                            );
+    }
+
+    public function addAction()
+    {
+        /**
+        * get data from our form
+        */
+        $catalogid          = $this->params()->fromPost('catalogid', FALSE);
+        $catalogname        = $this->params()->fromPost('catalogname', FALSE);
+        $catalogversion     = $this->params()->fromPost('catalogversion', FALSE);
+
+        $catalogRepository = $this->getEntityManager()->getRepository('Catalog\Entity\Catalog');
+
+        // initialize view result
+        $viewElements = array();
+
+        // in case we receive something from form
+        if($this->request->isPost() && $catalogname)
+        {
+
+            $catalog = $catalogRepository->find($catalogid);
+
+            // We cannot find catalog: create a new one
+            if(!$catalog)
+            {
+                $catalog = new \Catalog\Entity\Catalog();
+            }
+
+            // Change properties
+            $catalog->setName($catalogname);
+            $catalog->setVersion($catalogversion);
+
+            // apply modifications
+            $this->getEntityManager()->persist($catalog);
+
+            // save data to database
+            $this->getEntityManager()->flush();
+
+            // assign result to view
+            $viewElements = array('catalog' => $catalog);
+        }
+
+        return new ViewModel($viewElements);
     }
 
     public function getEntityManager()
